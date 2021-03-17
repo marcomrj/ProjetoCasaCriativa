592caefbed17a8e8b812e844838e439ebcfff9ca
Marco Antônio <marcomrj@outlook.com>
2021-03-17 04:36:36 -0300

    Criação do banco de dados por meio do SQLite, setagem da alocação para um servidor remoto e efetivação da possibilidade de adicionar ideias

--- public/script.js
+++ public/script.js
@@ -5,3 +5,29 @@ function onoff(){ /*Coloquei uma função que eu nomei de onoff, no modal, assim
         .classList
         .toggle("hide") /*Vai colocar e tirar dependendo do botão*/
 }
+
+function checkFields(event){
+
+    const valuesToCheck =[
+        "image",
+        "title",
+        "category",
+        "description",
+        "link"
+    ]
+
+    const isEmpty = valuesToCheck.find(function (value){
+        const checkIfIsString = typeof event.target[value].value === "string"
+        const checkIfIsEmpty = !event.target["title"].value.trim()
+        
+        if(checkIfIsString && checkIfIsEmpty){
+            return true
+        }
+        
+    })
+    console.log(isEmpty)
+    if(isEmpty){
+        event.preventDefault()
+        alert("Para continuar preencha todos os campos.")
+    }
+}
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express") //Passo o express para a variável express
 const server = express()            // Variável chama a função express
 server.use(express.static("public")) //Configura uma pasta para usar o estático dela CSS/IMAGENS/SCRIPTS
+server.use(express.urlencoded({extended: true})) //Libera disponibilidade do body no / POST
 server.listen(3000) //Porta que eu estou utilizando do host para hospedar o server
 
-//Array com os objetos que será utilizada para simplificar o codigo no índez do html
+const db = require("./db")
+
+/*/Array com os objetos que será utilizada para simplificar o codigo no índez do html
 const ideas = [
     {
         img: "https://image.flaticon.com/icons/svg/2729/2729007.svg",
@@ -33,26 +36,76 @@ const ideas = [
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus eaque voluptate quod incidun ",
         url: "https://linkedin.com/in/marcomrj"
     },
-]
+]*/
 
 server.get("/", function(req, res){
 
-    const reversedIdeas = [...ideas].reverse()
-    let lastIdeas = []
-    for(let idea of reversedIdeas){
-        if(lastIdeas.length < 2){
-            lastIdeas.push(idea)
+    db.all(`SELECT * FROM ideas`, function(err, rows){
+        if(err){
+            console.log(err)
+            return res.send("Erro no banco de dados!")
         }
-    }
-    return res.render("index.html",{ideas:lastIdeas}) //dirname busca no teu pc onde ta o arq, renderiza também os objetos ideas
+
+        const reversedIdeas = [...rows].reverse()
+
+        let lastIdeas = []
+        for(let idea of reversedIdeas){
+            if(lastIdeas.length < 2){
+                lastIdeas.push(idea)
+            }
+        }
+        return res.render("index.html",{ideas:lastIdeas}) //dirname busca no teu pc onde ta o arq, renderiza também os objetos ideas
+
+
+        console.log(rows)
+    })
+    
 })
 
 server.get("/ideias", function(req, res){
-    const reversedIdeas = [...ideas].reverse()
-
-    return res.render("ideias.html",{ideas:reversedIdeas})  //dirname busca no teu pc onde ta o arq
+    db.all(`SELECT * FROM ideas`, function(err, rows){
+        if(err) {
+            console.log(err)
+            return res.send("Erro no banco de cados !")
+        }
+        const reversedIdeas = [...rows].reverse()
+        return res.render("ideias.html",{ideas:reversedIdeas})  //dirname busca no teu pc onde ta o arq
+        console.log(rows)
+    })
+    
+    
+    
 })
 
+server.post("/", function(req,res){
+    //Insere dados na tabela
+    const query =`
+    INSERT INTO ideas(
+        image,
+        title,
+        category,
+        description,
+        link
+        )
+        VALUES(?,?,?,?,?);
+        `
+
+    const values=[
+        req.body.image,
+        req.body.title,
+        req.body.category,
+        req.body.description,
+        req.body.link
+    ]
+    db.run(query,values,function(err){
+        if(err){ 
+        console.log(err)
+        return console.log("Falha no banco de dados !")
+        }
+        
+        return res.redirect("/ideias")
+    })
+})
 const nunjucks= require("nunjucks")//Configuração do nunjucks
 nunjucks.configure("views",{
     express: server,
